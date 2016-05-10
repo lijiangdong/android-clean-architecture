192a8a47f0f6078e31eea63bb9e4769f8148a56b
Li Jiangdong <ljd2038@gmail.com>
2016-05-10 11:30:14 +0800

    commit

--- data/src/main/java/com/ljd/architecture/clean/data/net/RestApiImpl.java
+++ data/src/main/java/com/ljd/architecture/clean/data/net/RestApiImpl.java
@@ -1,33 +1,25 @@
 package com.ljd.architecture.clean.data.net;
 
-import android.content.Context;
-import android.util.Log;
-
 import com.ljd.architecture.clean.data.entity.ContributorEntity;
 import com.ljd.architecture.clean.data.entity.mapper.ContributorEntityJsonMapper;
 
 import java.io.IOException;
 import java.util.List;
 
-import okhttp3.ResponseBody;
 import rx.Observable;
-import rx.Observer;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
+
 
 /**
  * Created by ljd on 5/7/16.
  */
 public class RestApiImpl implements RestApi {
 
-    private final Context context;
     private final ContributorEntityJsonMapper contributorEntityJsonMapper;
 
-    public RestApiImpl(Context context, ContributorEntityJsonMapper contributorEntityJsonMapper) {
-        if (context == null || contributorEntityJsonMapper == null) {
+    public RestApiImpl(ContributorEntityJsonMapper contributorEntityJsonMapper) {
+        if (contributorEntityJsonMapper == null) {
             throw new IllegalArgumentException("The constructor parameters cannot be null!!!");
         }
-        this.context = context;
         this.contributorEntityJsonMapper = contributorEntityJsonMapper;
     }
 
@@ -37,8 +29,6 @@ public class RestApiImpl implements RestApi {
         return GitHubService
                 .getMspApiService()
                 .contributors("square","retrofit")
-                .subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
                 .flatMap(responseBody ->
                     Observable.create(subscriber -> {
                         try {
@@ -52,7 +42,6 @@ public class RestApiImpl implements RestApi {
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
-
                     }));
     }
 
